2cdd957ab3812582d7813a8b5870bdb8595e2a9c
Nikos Douvlis <nikosdouvlis@gmail.com>
2021-07-09 20:13:10 +0300

    Add <clerk-sign-up> component

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,29 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {ClerkService} from "../../services/clerk.service";
+
+@Component({
+  selector: 'clerk-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.sass']
+})
+export class SignUpComponent implements AfterViewInit {
+  @ViewChild('signUpContainer', {static: false}) private signUpContainer: ElementRef<HTMLDivElement> | undefined;
+
+  constructor(private clerk: ClerkService) { }
+
+  ngAfterViewInit() {
+    this.clerk.user$.subscribe(user => {
+      const el = this.signUpContainer?.nativeElement;
+      if(!el) {
+        return;
+      }
+
+      if(!user) {
+        this.clerk.mountSignUp(el)
+        return;
+      }
+
+      this.clerk.unmountSignUp(el);
+    })
+  }
+}
