eac178f27ed8ddd12f6b8cad396550d97b84acd8
Matej Bošnjak <mbosnjak2@outlook.com>
2021-07-22 17:25:59 +0200

    temporary production env push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  CLERK_FRONTEND_API: 'clerk.rv0d8.fjdqt.lcl.dev',
+  HARPERDB_API: 'https://flashcoll-mbosnjak.harperdbcloud.com'
 };
